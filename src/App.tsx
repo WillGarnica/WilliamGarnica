e83c7be80d4ff95540cf2013dcdeb42d1ea7af8c
William Garnica <wgarnicaw@gmail.com>
2023-07-28 03:22:58 +0000

    suport typescript add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App: React.FC = () => (
     <div className="App">
       <header className="App-header">
         <p>Hi. I'm <h2>William Garnica</h2></p>
-        <p><h1>Backend developer</h1></p>
+        <p><h1>Backend developer!</h1></p>
       </header>
     </div>
   );
-}
 
 export default App;
