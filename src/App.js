4537d8c6a9e4804b130574b401f8b4dff9bd5713
William Garnica <wgarnicaw@gmail.com>
2023-07-28 02:50:32 +0000

    readme edit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload. test
-        </p>
-        <p>
-          
-        </p>
+        <p>Hi. I'm <h2>William Garnica</h2></p>
+        <p><h1>Backend developer</h1></p>
       </header>
     </div>
   );
