0cf474a6f4bca39e054b88091306b01eaaedfafe
William Garnica <wgarnicaw@gmail.com>
2023-08-01 02:42:28 +0000

    linkedin button add

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
 
 const App: React.FC = () => (
-    <div className="App">
-      <header className="App-header">
-        <p>Hi. I'm <h2>William Garnica</h2></p>
-        <p><h1>Backend developer!</h1></p>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <header className="App-header">
+      <p>Hi. I'm <h2>William Garnica</h2></p>
+
+      <p><h1>Backend developer!</h1></p>
+
+      <Button variant="contained"
+        style={{ textTransform: 'none' }}
+        target="blank"
+        href="https://www.linkedin.com/in/william-garnica-70b93a196">
+        My Linkedin
+      </Button>
+
+
+    </header>
+  </div >
+);
 
 export default App;
