b8e113386e37a31c9625bb109a39ff7e86701a60
William Garnica <wgarnicaw@gmail.com>
2023-09-19 14:47:07 +0000

    modifications varios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Container, DialogActions, DialogTitle } from '@mui/material';
+import { Container, DialogActions, DialogTitle, List, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import React from 'react';
+import Avatar from '@mui/material/Avatar';
+
 import './App.css';
 
 const App: React.FC = () => {
@@ -15,18 +17,18 @@ const App: React.FC = () => {
 
         <p><h1>Backend developer!</h1></p>
 
-        <Button variant="contained"
-          style={{ textTransform: 'none' }}
-          target="blank"
-          href="https://www.linkedin.com/in/william-garnica-70b93a196">
-          My Linkedin
-        </Button>
+        <div>
+          <Button variant="contained"
+            style={{ textTransform: 'none' , margin:15 }}
+            target="blank"
+            href="https://www.linkedin.com/in/william-garnica-70b93a196">
+            My Linkedin
+          </Button>
 
-        <p></p>
-
-        <Button variant="outlined" onClick={() => { setOpenDialogAboutThisPage(true) }}>
-          About this page
-        </Button>
+          <Button variant="outlined" onClick={() => { setOpenDialogAboutThisPage(true) }}>
+            About this page
+          </Button>
+        </div>
 
         <Dialog
           onClose={() => { setOpenDialogAboutThisPage(false) }}
@@ -34,6 +36,7 @@ const App: React.FC = () => {
           <DialogTitle>About this page</DialogTitle>
           <Container>
             <p>App made in React v18.</p>
+            <p>Typescript: 5.1.6</p>
             <p>Deployed and published using Github Pages.</p>
             <p>Yes I know, sometimes I do things in the FrontEnd.</p>
             <DialogActions>
@@ -44,6 +47,48 @@ const App: React.FC = () => {
           </Container>
         </Dialog>
 
+        <br />
+        <br />
+        <h1>My knowledge</h1>
+
+        <List>
+          <ListItem style={{ backgroundColor: 'white' }}>
+            <ListItemAvatar>
+              <Avatar alt="J" src="%PUBLIC_URL%/logo192.png"   />
+            </ListItemAvatar>
+            <ListItemText primary="Java" secondary="10 years of experience" style={{ color: 'black' }} />
+          </ListItem>
+
+          <ListItem style={{ backgroundColor: 'white' }}>
+            <ListItemAvatar>
+              <Avatar alt="S" src="./img/linkedin_icon_135436.png" />
+            </ListItemAvatar>
+            <ListItemText primary="Spring Framework" secondary="5 years of experience" style={{ color: 'black' }} />
+          </ListItem>
+
+          <ListItem style={{ backgroundColor: 'white' }}>
+            <ListItemAvatar>
+              <Avatar alt="M" src="./src/img/linkedin_icon_135436.png" />
+            </ListItemAvatar>
+            <ListItemText primary="Microservices" secondary="5 years of experience" style={{ color: 'black' }} />
+          </ListItem>
+
+          <ListItem style={{ backgroundColor: 'white' }}>
+            <ListItemAvatar>
+              <Avatar alt="DP" src="../src/img/linkedin_icon_135436.png" />
+            </ListItemAvatar>
+            <ListItemText primary="Design patterns" secondary="7 years of experience" style={{ color: 'black' }} />
+          </ListItem>
+
+          <ListItem style={{ backgroundColor: 'white' }}>
+            <ListItemAvatar>
+              <Avatar alt="J" src="../public/linkedin_icon_135436.png" />
+            </ListItemAvatar>
+            <ListItemText primary="Junit / Mockito" secondary="5 years of experience" style={{ color: 'black' }} />
+          </ListItem>
+        </List>
+
+        <br />
 
       </header>
     </div >
